4cf237d4e2b5bf2ea1b14ac6f41a2e9a1d5a1076
Andrés Esguerra <kaosterra@gmail.com>
2015-01-28 10:19:43 -0500

    Refactorización
    Se cambia estructura y tipo de proyecto.

--- AngularBasic/public_html/src/modules/user/userController.js
+++ AngularBasic/public_html/src/modules/user/userController.js
@@ -4,10 +4,8 @@
 	app.controller('userController', ['$scope', function ($scope) {
 			$scope.user = {};
 			$scope.showForm = false;
-			$scope.buttonText = 'Mostrar Formulario';
-			this.toggleForm = function () {
-				$scope.showForm = !$scope.showForm;
-				$scope.buttonText = $scope.showForm && 'Esconder Formulario' || 'Mostrar Formulario';
+			this.showForm = function () {
+				$scope.showForm = true;
 			};
 			this.save = function () {
 				alert("Guardado: " + $scope.user.firstName + " " + $scope.user.lastName);
@@ -17,7 +15,7 @@
 	app.directive('userForm', [function () {
 			return {
 				restrict: 'E',
-				templateUrl: 'src/view/userTemplates.html'
+				templateUrl: 'src/modules/user/userTemplates.html'
 			};
 		}]);
 })();
